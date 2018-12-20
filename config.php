43f8c9162014663e6328440e4bba64582ed67ea1
Karel Jordaan <kareljordaan@yahoo.com>
2018-12-20 15:39:56 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-
-$db_host = "localhost";
-$db_user = "root";
-$db_password = "";
-$db_name = "reactdemo";
+//this is dummy info not actual database info
+$db_host = "xxxx";
+$db_user = "xxxx";
+$db_password = "xxxx";
+$db_name = "xxxx";
 $mysqli = mysqli_connect($db_host,$db_user,$db_password,$db_name);
 
 
